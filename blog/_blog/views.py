67e21c50983026c2ec51767b33915d892b6213df
Leul Degarege <leuldegarege11@gmail.com>
2023-07-21 12:41:25 +0300

    modified the home page to use a class-based view

--- blog/_blog/views.py
+++ blog/_blog/views.py
@@ -1,3 +1,4 @@
+from django.views.generic import ListView
 from django.shortcuts import render
 from .models import Post
 
@@ -8,6 +9,16 @@ def home(req):
         'posts' : Post.objects.all()
     }
     return render(req , '_blog/home.html', context)
+
+class PostListView(ListView):
+    model = Post
+    template_name = '_blog/home.html'
+    context_object_name = 'posts'
+    ordering = ['-date_posted']
+    
+
+
+
 
 def about(req):
     return render(req , "_blog/about.html", {"title": "About"})
