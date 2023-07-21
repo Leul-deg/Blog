2faac85368af30b304a6442f82e275cf3c61f521
Leul Degarege <leuldegarege11@gmail.com>
2023-07-21 13:32:08 +0300

    added the create posts functionality

--- blog/_blog/urls.py
+++ blog/_blog/urls.py
@@ -2,6 +2,7 @@ from django.urls import path
 from . import views
 urlpatterns = [
     path('', views.PostListView.as_view(), name='blog-home'),
+    path('post/new/', views.PostCreateView.as_view(), name='post-create'),
     path('post/<int:pk>/', views.PostDetailView.as_view(), name='post-detail'),
     path('about/', views.about, name='blog-about'),
 
--- blog/_blog/views.py
+++ blog/_blog/views.py
@@ -1,6 +1,7 @@
-from django.views.generic import ListView, DetailView
+from django.views.generic import ListView, DetailView, CreateView, UpdateView, DeleteView
 from django.shortcuts import render
 from .models import Post
+from django.contrib.auth.mixins import LoginRequiredMixin , UserPassesTestMixin
 
 def home(req):
 
@@ -20,8 +21,13 @@ class PostListView(ListView):
 class PostDetailView(DetailView):
     model = Post
      
+class PostCreateView(LoginRequiredMixin, CreateView):
+    model = Post
+    fields = ['title' , 'content']
 
-
+    def form_valid(self, form):
+        form.instance.author = self.request.user
+        return super().form_valid(form)
 
 def about(req):
     return render(req , "_blog/about.html", {"title": "About"})
