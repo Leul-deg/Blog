11ec6994a3f3efa721b91215774aa68c9ce3aa46
Leul Degarege <leuldegarege11@gmail.com>
2023-07-21 14:16:36 +0300

    added post update

--- blog/_blog/urls.py
+++ blog/_blog/urls.py
@@ -4,6 +4,7 @@ urlpatterns = [
     path('', views.PostListView.as_view(), name='blog-home'),
     path('post/new/', views.PostCreateView.as_view(), name='post-create'),
     path('post/<int:pk>/', views.PostDetailView.as_view(), name='post-detail'),
+    path('post/<int:pk>/update', views.PostUpdateView.as_view(), name='post-update'),
     path('about/', views.about, name='blog-about'),
 
 ]
--- blog/_blog/views.py
+++ blog/_blog/views.py
@@ -1,3 +1,4 @@
+from typing import Optional
 from django.views.generic import ListView, DetailView, CreateView, UpdateView, DeleteView
 from django.shortcuts import render
 from .models import Post
@@ -28,6 +29,20 @@ class PostCreateView(LoginRequiredMixin, CreateView):
     def form_valid(self, form):
         form.instance.author = self.request.user
         return super().form_valid(form)
+
+class PostUpdateView(LoginRequiredMixin, UserPassesTestMixin,  UpdateView):
+    model = Post
+    fields = ['title' , 'content']
+
+    def form_valid(self, form):
+        form.instance.author = self.request.user
+        return super().form_valid(form)
+    
+    def test_func(self):
+        post = self.get_object()
+        if self.request.user == post.author:
+            return True
+        return False
 
 def about(req):
     return render(req , "_blog/about.html", {"title": "About"})
