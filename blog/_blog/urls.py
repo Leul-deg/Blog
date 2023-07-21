14f7b1109078e4355ff61358f2d71457e903a443
Leul Degarege <leuldegarege11@gmail.com>
2023-07-21 16:23:47 +0300

    added a page to display posts from a specific user

--- blog/_blog/urls.py
+++ blog/_blog/urls.py
@@ -6,6 +6,7 @@ urlpatterns = [
     path('post/<int:pk>/', views.PostDetailView.as_view(), name='post-detail'),
     path('post/<int:pk>/update', views.PostUpdateView.as_view(), name='post-update'),
     path('post/<int:pk>/delete', views.PostDeleteView.as_view(), name='post-delete'),
+    path('user/<str:username>', views.UserPostListView.as_view(), name='user-post'),
     path('about/', views.about, name='blog-about'),
 
 ]
